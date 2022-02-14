c1469cf296865ac0526580c02ce97e8b01e17d14
정수진 <sssjsjj@ipartners.co.kr>
2022-02-14 23:35:13 +0900

    eslint, webpack 셋팅

--- FastCampus/vue3-webpack-template/src/App.vue
+++ FastCampus/vue3-webpack-template/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>{{ message }} </h1>
+  <hello-world />
 </template>
 
 <script>
+import HelloWorld from '~/components/HelloWorld'
 export default {
+  components: {
+    HelloWorld
+  },
   data() {
     return {
       message: 'hello vue'
---
+++ FastCampus/vue3-webpack-template/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    src="~assets/logo.png"
+    alt="" />
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
